abc0741c07eb0d26cad7bc62adb7cce4a63d1792
Glen357 <161297213+Glen357@users.noreply.github.com>
2024-09-20 23:40:10 +1200

    Added footer component folder, full screen styling complete, apart from new button. RWD for Footer 90% done just need to rearange stack at base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
+import {  BrowserRouter as Router,  Routes,  Route,  Navigate,} from "react-router-dom";
 import './App.css'
 import Homepage from './homepage/homepage'
 
 function App() {
 
   return (
+    <>
      <div className='layout-main-page'>
       <Homepage />
-
      </div>
+         <div>
+           <Router>
+             <Routes>
+             <Route path="/" element={<Navigate to="/Home" />} /> */This is how I
+             made App.jsx the home page /*
+               <Route path="/homepage" Component={Homepage} />
+               {/* <Route path="/Page_Options" Component={Page_Options} />
+               <Route path="/Portfolio" Component={Portfolio} />
+               <Route path="/Contact" Component={Contact} />
+               <Route path="/content/:id" element={<Info_page />} /> */}
+             </Routes>
+           </Router>
+         </div>
+       </>
   )
 }
 
 export default App
+
--- frontend/src/homepage/homepage.jsx
+++ frontend/src/homepage/homepage.jsx
@@ -2,12 +2,16 @@ import Navbar from "../navbar/navbar";
 import SearchComponent from "../search-component/search-component";
 import CategoryComponent from "../category-component/category-component";
 
+import Footer from "../Footer/footer";
+
 const Homepage = () => {
     return (
         <>
         <Navbar />
         <SearchComponent />
         <CategoryComponent />
+
+        <Footer />
         </>
     )
 }
