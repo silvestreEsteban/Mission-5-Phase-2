e379a5f8f1c5fdfc91224db80f350fa34191029e
Silvestre Esteban Fernández Liñero <silvestref@missionreadyhq.com>
2024-09-20 16:56:43 +1200

    Created category component and made mobile responsive

--- frontend/src/homepage/homepage.jsx
+++ frontend/src/homepage/homepage.jsx
@@ -1,9 +1,13 @@
 import Navbar from "../navbar/navbar";
+import SearchComponent from "../search-component/search-component";
+import CategoryComponent from "../category-component/category-component";
 
 const Homepage = () => {
     return (
         <>
         <Navbar />
+        <SearchComponent />
+        <CategoryComponent />
         </>
     )
 }
