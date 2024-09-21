1607e340f0ca6fa8b3efad2535a40c9db4deee42
Glen357 <161297213+Glen357@users.noreply.github.com>
2024-09-21 15:24:04 +1200

    set up server.js with endpoint for api endpoint - have .env in gitignore - Api ready for Jyothy to adjust for listing calls

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB Connect"))
+.catch(err => console.error(err));
+
+app.use(cors());
+app.use(express.json());
+
+const schema = new mongoose.Schema({
+    image: String,
+    title: String,
+    location: String,
+    condition: String,
+    size: String,
+    price: Number,
+    colour: String,
+    dimensions: String,
+  });
+
+  const Listing = mongoose.model("Listing", schema);
+
+  app.get("/api/listings", async (req, res) => {
+    try {
+        constlistings = Listings.find();
+        res.status(200).json(listings);
+    }catch (error) {
+        res.status(500).json({message: "Error getting listings", error});
+    }
+  });
+
+app.listen(PORT, () => {
+    console.log("Server running at http://localhost:${PORT}")
+});
