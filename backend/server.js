658a7a7eac3274c9b6d605455d4bd289303247df
Glen357 <161297213+Glen357@users.noreply.github.com>
2024-09-24 21:50:02 +1200

    adjusted footer fitting fo Silvestres page

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import { clockSVG } from "../frontend/src/components/productListingContent/productListingSVG";
 // import productRouter from "../Router/productRouter.js";
 dotenv.config();
 
